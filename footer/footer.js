8b34eaff2f0e5e2622b3944d72d0640fa84a32a8
lglopez <lglopez@scl.edu.gt>
2024-05-30 11:25:26 -0600

    version de maquetacion_2 gl

--- footer/footer.js
+++ footer/footer.js
@@ -1,2 +1,5 @@
 let gl_footer = document.querySelector(".footer");
-gl_footer.innerHTML = `Hola soy footer`;
+gl_footer.innerHTML = `
+    <h6 class="logo-ft">SCL</h6>
+    <a class="correo-ft" href="">@lglopez@scl.edu.gt</a>
+`;
--- header/header.js
+++ header/header.js
@@ -1,2 +1,5 @@
 let gl_header = document.querySelector(".header");
-gl_header.innerHTML = `Hola soy header`;
+gl_header.innerHTML = `
+    <h1 class="title">Gramatica HTML</h1>
+    <button class="btn">git</button>
+`;
