9a3606abab3d635a722ff4e9fa5b1dcd771fe510
Santiago <slopez@scl.edu.gt>
2024-05-30 19:03:42 -0600

    trabajo de la seccion 1

--- section1/section1.js
+++ section1/section1.js
@@ -1,2 +1,58 @@
 let gl_section1 = document.querySelector(".section1");
-gl_section1.innerHTML = `Hola soy sección 1`;
+gl_section1.innerHTML = `
+
+<table border="1">
+        <tr>
+            <td>1</td>
+            <td> "div"</td>
+            <td>Define una sección genérica o contenedor.</td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>"p"</td>
+            <td>Define un párrafo.</td>
+        </tr>
+        <tr>
+            <td>3</td>
+            <td>"h1 a h6"</td>
+            <td>Define encabezados de diferentes niveles.</td>
+        </tr>
+        <tr>
+            <td>4</td>
+            <td>"header"</td>
+            <td>Define el encabezado de una sección o de un documento.</td>
+        </tr>
+        <tr>
+            <td>5</td>
+            <td>"footer"</td>
+            <td>Define el pie de página de una sección o de un documento.</td>
+        </tr>
+        <tr>
+            <td>6</td>
+            <td>nav</td>
+            <td>Define la navegación en un documento.</td>
+        </tr>
+        <tr>
+            <td>7</td>
+            <td>section</td>
+            <td>Define una sección en un documento.</td>
+        </tr>
+        <tr>
+            <td>8</td>
+            <td>article</td>
+            <td>  Define un contenido independiente que puede existir por sí mismo.</td>
+        </tr>
+        <tr>
+            <td>9</td>
+            <td>main</td>
+            <td>Define el contenido principal de un documento.</td>
+        </tr>
+        <tr>
+            <td>10</td>
+            <td>blockquote</td>
+            <td>Define una cita en bloque que proviene de otra fuente.</td>
+        </tr>
+
+        
+    </table>
+`;
